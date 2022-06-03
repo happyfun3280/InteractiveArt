9b96e75fd8ed0c3ca2d72a513ad5d77f8fc595af
happyfun <happyfun3280@gmail.com>
2022-06-03 21:08:41 +0900

    2022.6.3. 21:00

---
+++ arts/balloonArt.js
@@ -0,0 +1,51 @@
+class BalloonArt extends Art {
+    constructor(red, blue, green) {
+        super(red, blue, green);
+        this.CHANGE_LEVEL = 8;
+        this.circleSize = 80;
+        this.color_R = 0;
+        this.color_G = 0;
+        this.color_B = 0;
+        this.count_circle = 0;
+        this.repeat_check = 0;
+    }
+
+    setup(p) {
+        super.setup(p);
+        p.noStroke();
+    }
+
+    draw(p) {
+        p.background(this.bgc.r, this.bgc.b, this.bgc.r);
+        console.log(this.bgc);
+
+        if(p.mouseX<Gallery.getInst().canvasWidth/2 && p.mouseY<Gallery.getInst().canvasHeight/2){
+            this.color_R = 255;
+            this.color_G = 200;
+            this.color_B = 200;
+        }else if(p.mouseX>=Gallery.getInst().canvasWidth/2 && p.mouseY<Gallery.getInst().canvasHeight/2){
+            this.color_R = 0;
+            this.color_G = 200;
+            this.color_B = 255;
+        }else if(p.mouseX<Gallery.getInst().canvasWidth/2 && p.mouseY>=Gallery.getInst().canvasHeight/2){
+            this.color_R = 200;
+            this.color_G = 255;
+            this.color_B = 100;
+        }else{
+            this.color_R = 150;
+            this.color_G = 80;
+            this.color_B = 255;
+        }
+
+        if (p.mouseIsPressed) {
+            this.circleSize += 2;
+            for(this.repeat_check = 0; this.repeat_check <= (this.circleSize/100) + 1; this.repeat_check++){
+                p.fill(this.color_R - (this.CHANGE_LEVEL*this.repeat_check), this.color_G - (this.CHANGE_LEVEL*this.repeat_check), this.color_B - (this.CHANGE_LEVEL*this.repeat_check));
+                p.ellipse(p.mouseX,p.mouseY, this.circleSize - (100 * this.repeat_check), this.circleSize - (100 * this.repeat_check));
+            }
+        }
+        else {
+            this.circleSize = 0;
+        }
+    }
+}
